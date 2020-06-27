aaa218fd5dbdb0c286be264458d3232753984eda
Amanda <neelyab@vcu.edu>
2020-06-27 13:25:04 -0400

    added dummy store and state in search bar

---
+++ src/DummyStore.js
@@ -0,0 +1,74 @@
+const Store = [
+    { 
+        recipe: {
+            label:"Chicken Paprikash",
+            image: "https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg",
+            url:"http://norecipes.com/recipe/chicken-paprikash/", 
+            dietLabels:["Low-Carb"],
+            healthLabels:[
+                "Peanut-Free", 
+                "Tree-Nut-Free"
+            ],
+        }
+    },
+    {
+        recipe: {
+            label:"Vegan Chocolate-Coffee Muffins Recipe",
+            image: "https://www.edamam.com/web-img/cd0/cd0f763c1566ec5a0f0ae099ecebc5a7.jpg",
+            url: "http://www.seriouseats.com/recipes/2014/04/vegan-chocolate-coffee-muffins.html", 
+            dietLabels:[],
+            healthLabels:[
+                    "Vegetarian",
+                    "Peanut-Free", 
+                    "Tree-Nut-Free", 
+                    "Alcohol-Free"
+                ],
+        }
+    },
+    {
+        recipe: {
+            label:"Vegan Meatballs",
+            image: "https://www.edamam.com/web-img/8b3/8b38d9a6c0dea85e443307459200f51f.jpg",
+            url: "https://www.marthastewart.com/1120262/vegan-meatballs", 
+            dietLabels:["Balanced"],
+            healthLabels:[
+                    "Sugar-Conscious", 
+                    "Vegan", 
+                    "Vegetarian",
+                    "Peanut-Free", 
+                    "Tree-Nut-Free", 
+                    "Alcohol-Free"
+                ],
+            }
+    },
+    {
+        recipe: {
+            label: "Vegan Broccoli Soup Recipe",
+            image: "https://www.edamam.com/web-img/8c8/8c8506a5b2f074b7a5aa148710eb85a8.jpg",
+            url: "http://www.foodrepublic.com/2012/03/28/classic-american-fare-now-vegan", 
+            dietLabels: ["Low-Carb"],
+            healthLabels: [
+                    "Vegetarian",
+                    "Peanut-Free", 
+                    "Alcohol-Free"
+                ],
+            }
+    },
+    { 
+        recipe: {
+            label: "Keto Chocolate Smoothie recipes",
+            image: "https://www.edamam.com/web-img/083/0834f93ea42c9df7e2faf4125f2b13c1",
+            url: "https://elanaspantry.com/keto-chocolate-smoothie/",
+            dietLabels: [
+                "Low-Carb"
+            ],
+            healthLabels: [
+                "Vegetarian",
+                "Peanut-Free",
+                "Tree-Nut-Free",
+                "Alcohol-Free"
+            ],
+        }   
+    }
+
+]
--- src/Search/Search.js
+++ src/Search/Search.js
@@ -2,30 +2,81 @@ import React, {Component} from 'react';
 import './Search.css'
 
 class Search extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            peanut: false,
+            sugar: false,
+            treeNut: false,
+            dietOne: null,
+            dietTwo: null,
+            searchTerm: null,
+            otherPreferences: []
+        }
+    }
+    handleSugar = () => {
+       this.setState({
+           sugar: !this.state.sugar
+       })
+    }
+    handlePeanut = () => {
+        this.setState({
+            peanut: !this.state.peanut
+        })
+     }
+     handleTreeNut = () => {
+        this.setState({
+            treeNut: !this.state.treeNut
+        })
+     }
+    handleSearch = (value) => {
+        this.setState({
+            searchTerm: value
+        })
+    }
+    handleDietOne = (e) => {
+        this.setState({
+            dietOne: e.target.value
+        })
+    }
+    handleDietTwo = (e) => {
+        this.setState({
+            dietTwo: e.target.value
+        })
+    }
     render(){
         return(
             <form className="meal-search">
                 <section className="search">
                 <p>Search Recipes</p>
-                <input type="text"></input>
+                <input type="text" onChange={(e)=>this.handleSearch(e.target.value)} required></input>
                 <label htmlFor="diet">
                     Select 2 diets:
                 </label>
-                <select className="diet" name="diet-1" id="diet">
-                    <option>Select One</option>
-                    <option>Keto</option>
-                    <option>Paleo</option>
-                    <option>Vegan</option>
-                    <option>Vegetarian</option>
-                    <option>Gluten Free</option>
-                        <option></option>
+                <select className="diet" name="diet-1" id="diet" onChange={(e)=>this.handleDietOne(e)} required>
+                    <option name="select-one" value={null}>Select One</option>
+                    <option name="balanced" value="balanced">Balanced</option>
+                    <option name="high-protein" value="high-protein">High-Protein</option>
+                    <option name="low-carb" value="low-carb">Low-Carb</option>
+                    <option name="low-fat" value="low-fat">Low-Fat</option>
+                    <option name="vegan" value="vegan">Vegan</option>
+                    <option name="vegetarian" value="vegetarian">Vegetarian</option>
                 </select>
-                <select className="diet" name="diet-2">
-                    <option>Select One</option>
-                    <option>Low-Carb</option>
-                    <option>Vegan</option>
-                    <option>Vegetarian</option>
+                <select className="diet" name="diet-2" onChange={(e)=>this.handleDietTwo(e)} required>
+                    <option name="select-one" value={null}>Select One</option>
+                    <option name="balanced" value="balanced">Balanced</option>
+                    <option name="high-protein" value="high-protein">High-Protein</option>
+                    <option name="low-carb" value="low-carb">Low-Carb</option>
+                    <option name="low-fat" value="low-fat">Low-Fat</option>
+                    <option name="vegan" value="vegan">Vegan</option>
+                    <option name="vegetarian" value="vegetarian">Vegetarian</option>
                 </select>
+                <label htmlFor="sugar-conscious">Sugar Conscious</label>
+                <input type="checkbox" name="sugar" value="sugar-conscious" checked={this.state.sugar} onChange={()=> this.handleSugar()}></input>
+                <label htmlFor="tree-nut-free">Tree Nut Free</label>
+                <input type="checkbox" name="tree-nut-free" value="tree-nut-free" checked={this.state.treeNut} onChange={()=> this.handleTreeNut()}></input>
+                <label htmlFor="peanut-free">Peanut Free</label>
+                <input type="checkbox" name="peanut-free" value="peanut-free" checked={this.state.peanut} onChange={()=> this.handlePeanut()}></input>
                 <button type="submit" className="submit" >Submit</button>
                 </section>
               </form>
