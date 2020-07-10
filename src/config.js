d2aa795bfab1c0346b5324a4b8379af0f2993942
Amanda <neelyab@vcu.edu>
2020-07-10 17:01:15 -0400

    fixed api endpoint url

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export default {
     RECIPE_API_KEY: process.env.REACT_APP_API_KEY,
     APP_ID: process.env.REACT_APP_ID,
     RECIPE_URL: "https://api.edamam.com/search?",
-    API_ENDPOINT: "https://boiling-dawn-47143.herokuapp.com/"
+    API_ENDPOINT: "https://boiling-dawn-47143.herokuapp.com"
     
 }
